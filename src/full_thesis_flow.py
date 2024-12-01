e4b68c461b363ca7e481f66ce8174ddff91065b3
Laxman Bhattarai <erluxman@gmail.com>
2024-12-02 04:21:11 +0545

    uncomment some flow

--- src/full_thesis_flow.py
+++ src/full_thesis_flow.py
@@ -4,18 +4,18 @@ from src.steps.compose_data import *
 
 
 def prepareData():
-    # delete_output_directories()
+    delete_output_directories()
     download_data()
     normalize_data()
 
 
 def calculate_candleSticks():
     calculate_candleSticks_us()
-    # calculate_candleSticks_np()
+    calculate_candleSticks_np()
 
 def compose_transformation_result():
     export_to_excel_us()
-    # export_to_excel_np()
+    export_to_excel_np()
     export_summary_to_json_us()
-    # export_summary_to_json_np()
+    export_summary_to_json_np()
     
