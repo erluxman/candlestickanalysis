7883e8cb793e4ed0fdcf4850c9e4d3c0248703a1
Laxman Bhattarai <erluxman@gmail.com>
2025-01-26 21:17:02 +0545

    now the main table is generated bearish random candle to be computed

--- src/steps/compute_descriptive_stats.py
+++ src/steps/compute_descriptive_stats.py
@@ -86,10 +86,100 @@ def compute_category_analytics():
                     for duration in durations:
                         analytics[sector][trend][duration] = {}
                         for candle_id, candle_name in pattern_with_names.items():
-                            data_of_candle = pattern_data[candle_name]
-                            data_following_trend = data_following_trend = [
+                            all_data = pattern_data[candle_name]
+                            data_following_trend = [
                                 item
-                                for item in data_of_candle
+                                for item in all_data
+                                if (
+                                    item.get("meta_data", {})
+                                    .get("trend_past", {})
+                                    .get(str(duration), {})
+                                    .get("trend_present", False)
+                                )
+                            ]
+
+                            hit_high_all = []
+                            for item in all_data:
+                                ma_future = item.get("meta_data", {}).get(
+                                    "ma_future", {}
+                                )
+                                ma_future_high = ma_future.get("High", {})
+                                ma_future_high_duration = ma_future_high.get(
+                                    str(duration), {}
+                                )
+                                if not ma_future_high_duration:
+                                    continue
+                                candle_approved_from_point = (
+                                    ma_future_high_duration.get(
+                                        "candle_approved_from_point", False
+                                    )
+                                )
+                                if candle_approved_from_point:
+                                    hit_high_all.append(item)
+
+                            hit_low_all = []
+                            for item in all_data:
+                                ma_future = item.get("meta_data", {}).get(
+                                    "ma_future", {}
+                                )
+                                ma_future_high = ma_future.get("Low", {})
+                                ma_future_high_duration = ma_future_high.get(
+                                    str(duration), {}
+                                )
+                                if not ma_future_high_duration:
+                                    continue
+                                candle_approved_from_point = (
+                                    ma_future_high_duration.get(
+                                        "candle_approved_from_point", False
+                                    )
+                                )
+                                if candle_approved_from_point:
+                                    hit_low_all.append(item)
+
+                            hit_close_all = []
+                            for item in all_data:
+                                ma_future = item.get("meta_data", {}).get(
+                                    "ma_future", {}
+                                )
+                                ma_future_high = ma_future.get("Close", {})
+                                ma_future_high_duration = ma_future_high.get(
+                                    str(duration), {}
+                                )
+                                if not ma_future_high_duration:
+                                    continue
+                                candle_approved_from_point = (
+                                    ma_future_high_duration.get(
+                                        "candle_approved_from_point", False
+                                    )
+                                )
+                                if candle_approved_from_point:
+                                    hit_close_all.append(item)
+
+                            hit_high_trend = [
+                                item
+                                for item in hit_high_all
+                                if (
+                                    item.get("meta_data", {})
+                                    .get("trend_past", {})
+                                    .get(str(duration), {})
+                                    .get("trend_present", False)
+                                )
+                            ]
+
+                            hit_low_trend = [
+                                item
+                                for item in hit_low_all
+                                if (
+                                    item.get("meta_data", {})
+                                    .get("trend_past", {})
+                                    .get(str(duration), {})
+                                    .get("trend_present", False)
+                                )
+                            ]
+
+                            hit_close_trend = [
+                                item
+                                for item in hit_close_all
                                 if (
                                     item.get("meta_data", {})
                                     .get("trend_past", {})
@@ -99,23 +189,60 @@ def compute_category_analytics():
                             ]
 
                             occurance_trend = len(data_following_trend)
-                            all_occurance = len(data_of_candle)
-                            hit_percentage_high_trend = 60
-                            hit_percentage_high_all = 40
-                            hit_percentage_low_trend = 70
-                            hit_percentage_low_all = 50
-                            hit_percentage_close_trend = 60
-                            hit_percentage_close_all = 40
+                            all_occurance = len(all_data)
+                            hit_percentage_high_trend = (
+                                len(hit_high_trend) / occurance_trend * 100
+                                if occurance_trend
+                                else 0
+                            )
+                            hit_percentage_high_all = (
+                                len(hit_high_all) / all_occurance * 100
+                                if all_occurance
+                                else 0
+                            )
+                            hit_percentage_low_trend = (
+                                len(hit_low_trend) / occurance_trend * 100
+                                if occurance_trend
+                                else 0
+                            )
+                            hit_percentage_low_all = (
+                                len(hit_low_all) / all_occurance * 100
+                                if all_occurance
+                                else 0
+                            )
+                            hit_percentage_close_trend = (
+                                len(hit_close_trend) / occurance_trend * 100
+                                if occurance_trend
+                                else 0
+                            )
+                            hit_percentage_close_all = (
+                                len(hit_close_all) / all_occurance * 100
+                                if all_occurance
+                                else 0
+                            )
 
                             analytics[sector][trend][duration][candle_name] = {
                                 "occurance_trend": occurance_trend,
                                 "all_occurance": all_occurance,
-                                "hit_percentage_high_trend": hit_percentage_high_trend,
-                                "hit_percentage_high_all": hit_percentage_high_all,
-                                "hit_percentage_low_trend": hit_percentage_low_trend,
-                                "hit_percentage_low_all": hit_percentage_low_all,
-                                "hit_percentage_close_trend": hit_percentage_close_trend,
-                                "hit_percentage_close_all": hit_percentage_close_all,
+                                "hit_percentage_high_trend": round(
+                                    hit_percentage_high_trend, 2
+                                ),
+                                "hit_percentage_high_all": round(
+                                    hit_percentage_high_all, 2
+                                ),
+                                "hit_percentage_low_trend": round(
+                                    hit_percentage_low_trend, 2
+                                ),
+                                "hit_percentage_low_all": round(
+                                    hit_percentage_low_all,
+                                    2,
+                                ),
+                                "hit_percentage_close_trend": round(
+                                    hit_percentage_close_trend, 2
+                                ),
+                                "hit_percentage_close_all": round(
+                                    hit_percentage_close_all, 2
+                                ),
                             }
             analytics[sector][trend]["commentry"] = get_analytics_commentry(
                 analytics[sector][trend]
@@ -129,6 +256,6 @@ def get_analytics_commentry(data):
 
 
 def compute_descriptive_stats():
-    merge_jsons()
-    categorize_stats()
+    # merge_jsons()
+    # categorize_stats()
     compute_category_analytics()
