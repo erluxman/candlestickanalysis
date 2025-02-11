0ac530c6f0ce9791b459ab87c46a2fcd7002e47d
Laxman Bhattarai <erluxman@gmail.com>
2025-02-11 16:59:47 +0545

    WIP

--- index.py
+++ index.py
@@ -4,8 +4,8 @@ from src.steps.conclusion import printAverageSummariesForCandles
 from src.writer.thesis_writer import write_thesis
 from src.steps.compute_descriptive_stats import *
 
-# prepareData()
-# calculate_candleSticks()
-# ##### compose_transformation_result()
-# compute_descriptive_stats()
+prepareData()
+calculate_candleSticks()
+##### compose_transformation_result()
+compute_descriptive_stats()
 write_thesis()
